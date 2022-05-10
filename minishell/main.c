74c13fabdb55cb5308ae12b159495008d57a05a5
Sumin Wi <swi@c2r5s8.42seoul.kr>
2022-05-10 17:31:02 +0900

    every

--- minishell/main.c
+++ minishell/main.c
@@ -1,87 +1,130 @@
 #include "minishell.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <sys/wait.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <termios.h>
 
-/*
- * redirection처리시 남은 부분. 
- * 1. 구조체(이름모름)에서 받은 값을 red_open_file, red_pre_execute 잘 처리해주기
- * 2. 에러 제대로 출력.
- * 3. 출력값 redirection 처리
- * 4. redirection 처리하면서 만든 fd들 close해줘야함. 
- * 4-1. 마지막 in 아니면 바로 close. here_doc & in은 execute 후에 바로 close. 
- * 4-2. 마지막 out 아니면 바로 close.
- * 5. stdout 따로 저장해줘야할까? 그냥 쓰면 될까?
- *
- * 출력은 3가지 경우로 나눔.
- * 우선 출력용 fd 하나를 두고, 그 fd값에 red~ 또는 pipe를 넣어주고
- * stdout이랑 연결
- * 파이프와 red~가 없다면 dup_check 없이 바로 stdout
- */
+// 매개변수로 명령어를 입력하여 토큰화된 결과를 출력.
+// ex
+// >> "ls -al > a | grep abc"  (readline 함수에서 명령어를 한 문자열을 받는 것을 고려하여 전체를 한 문자열로 묶어서 전달해야함.)
+// ls
+// token: T_WORD
+// type: ARGS
+// ----------
+// -al
+// token: T_WORD
+// type: ARGS
+// ----------
+// a
+// token: T_RE_OUTPUT
+// type: REDIRECT
+// ----------
+// |
+// token: T_PIPE
+// type: PIPE
+// ----------
+// grep
+// token: WORD
+// type: ARGS
+// ----------
+// abc
+// token: WORD
+// type: ARGS
+// ----------
 
-int main(int argc, char *argv[], char *env[])
+void	handler(int signum)
 {
-	t_envs	e;
-	int		fd;
-	int		i;
-	t_paths	p;
-	t_oper	o;
-	char	*input_oper;
-	int		pipe_fd[2];
-	int		status;
-
-	if (input_env(&e, env) == 1)
-		return (1);
-	// 여기서부터 redirection 부분.
-
-	/* >> 시스템 함수 실행시 pipe close()할 때를 위해 init() */
-	//init_pipe(pipe_fd);
-	/* >> 만약 pipe가 있으면 미리 여기서 pipe 생성 후 out redirection 연결*/
-	//make_pipe(pipe_fd);
-	/* >> pipe가 없으면 stdout 다시 제대로 연결 */
-	//dup_check(STDOUT_FILENO, STDOUT_FILENO);
-
-	/* >> redirection open && dup2 && close(fd) */
-	//if (red_open_file(T_RE_HEREDOC, "a") == WRONG_ACTION)
-		/*error check 후에 종료 또는 다음 pipe 실행*/
-
-	find_path(&(p.paths), &(p.max_len));
-	input_oper = argv[1];
-	o.oper_path = make_oper(&o.opers, p.max_len, p.paths, input_oper);
-	
-	// 파이프 유무 체크 후 확인.
-	// 파이프가 있으면 cd 도 동작 안함.(이동하지 않음)
-	// 즉 파이프가 있으면 무조건 fork해서 실행된다.
-	
-	// 아래 함수 부터 수정하면 됨.
-	/*===========================================================*/
-	/*
-	 * if (파이프 있으면)
-	 * 		자식 생성 함수
-	 * else
-	 * 		if 빌트인 함수면
-	 * 			빌트인 함수 실행
-	 * 		else
-	 * 			시스템 함수 실행
-	 */
-	/*===========================================================*/
-	if (built_in_check(&o, &e) == -1)
+	if (signum == SIGINT)
 	{
-		if (o.oper_path == NULL)
-			printf("bash: %s: %s\n", o.opers[0], COMMAND_ERROR); // exit:127
-		else
-			exe_oper(&o, pipe_fd, e.env);
+		write(STDOUT_FILENO, "\n", 1);
+		if (rl_on_new_line() == -1)
+			exit(1);
+		rl_replace_line("", 1);
+		rl_redisplay();
 	}
-	/*===========================================================*/
-	while (waitpid(-1, &status, 0) >= 0)
-		;
-	// 만약 다음 파이프가 없고 status == 11 이라면 아래의 값 출력
-		//printf("Segmentation fault: %d\n", status);
-		//exit code: 128+11=139
-	free_double_char(o.opers);// 매 번들마다.
-	free(o.oper_path);//매 번들마다.
+}
 
-	free_envs(&e);//bash 종료시에.
-	free_double_char(p.paths);// bash 종료시에.
+void	init_term(void)
+{
+	struct termios	term;
+
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag &= ~ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+
+void	init_token_header(t_token **token_header)
+{
+	*token_header = malloc(sizeof(t_token));
+	if (*token_header == NULL)
+		exit(1);
+	(*token_header)->s = NULL;
+	(*token_header)->next = NULL;
+}
+
+void	init_tree(t_ast **tree)
+{
+	*tree = malloc(sizeof(t_ast));
+	if (tree == NULL)
+		exit(1);
+	(*tree)->token = NULL;
+	(*tree)->left = NULL;
+	(*tree)->right = NULL;
+	(*tree)->tree_type = TREE_PIPE;
+	(*tree)->pipe_cnt = 0;
+	(*tree)->root = *tree;
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	char			*line;
+	t_token			*token_header; // token 리스트의 헤더.
+	t_ast			*tree;
+	t_envs			env;
+
+	if (argc > 1)
+		return (1);
+	(void)argv;
+	(void)envp;
+	init_term();
+	init_env(&e, env);
+	signal(SIGINT, handler);
+	signal(SIGQUIT, SIG_IGN);
+	while (true)
+	{
+		line = readline("minishell> ");
+		if (line)
+		{
+			init_token_header(&token_header);
+			if (tokenizing(line, token_header) == WRONG_ACTION)
+			{
+				printf("detected unclosed quote\n");
+				free_token(token_header);
+			}
+			else
+			{
+				print_token_list(token_header);
+				init_tree(&tree);
+				parsing(tree, token_header);
+				free_token(token_header);
+				free_tree(tree);
+			}
+			add_history(line);
+			free(line);
+			line = NULL;
+		}
+		else
+		{
+			printf("\033[1A");
+			printf("\033[11C");
+			printf("exit\n");
+			return (1);
+		}
+	}
 	return (0);
 }
---
+++ minishell/tokenize/tokenize.c
@@ -0,0 +1,268 @@
+#include "../minishell.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*get_env(char **start, char *end)
+{
+	char	*key;
+
+	key = ft_strndup(*start, end - *start);
+	free(key);
+	return (ft_strndup("value", 5));
+}
+
+char	*split_word_in_dollar(char **start, char **end, enum e_token *token)
+{
+	char	*s;
+	char	*value;
+
+	++(*end);
+	if (**end == 0 || ft_strchr(" \"\'|><\\$", **end))
+	{
+		s = ft_strdup("$");
+	}
+	else if (**end == '?')
+	{
+		s = ft_strndup("$?", 2);
+		*token |= T_EXIT_CODE;
+		++(*end);
+	}
+	else
+	{
+		while (**end && !ft_strchr_ig_blsh(" \"\'|><\\$", *end, end))
+			++(*end);
+		value = get_env(start, *end);
+		s = ft_strndup(value, ft_strlen(value));
+		free(value);
+	}
+	return (s);
+}
+
+char	*split_word_in_quote(char **start, char **end, enum e_token *token)
+{
+	char	*s;
+	char	*d_end;
+	char	*value;
+	char	quote;
+
+	s = NULL;
+	quote = **start;
+	++(*start);
+	while (**start != **end || (**start == quote && *(*start - 1) == '\\'))
+	{
+		if (**start == '$' && quote == '\"')
+		{
+			d_end = *start + 1;
+			if (*d_end == '?')
+			{
+				*token |= T_EXIT_CODE;
+				++d_end;
+				s = ft_strnjoin(s, "$?", 2);
+			}
+			else
+			{
+				while (*d_end && !ft_strchr_ig_blsh(" \"\'|><\\$", d_end, &d_end))
+					++d_end;
+				value = get_env(start, d_end);
+				s = ft_strnjoin(s, value, ft_strlen(value));
+				free(value);
+			}
+			*start = d_end - 1;
+		}
+		else
+			s = ft_strnjoin(s, *start, 1);
+		++(*start);
+	}
+	if (s == NULL)
+		s = ft_strdup("");
+	++(*end);
+	return (s);
+}
+
+void	move_quote_end(char **start, char **end)
+{
+	while (**end)
+	{
+		if (**end == **start)
+		{
+			if (*(*end - 1) != '\\')
+				break ;
+		}
+		++(*end);
+	}
+}
+
+t_token	*split_word(char **start, char **end, enum e_token *token)
+{
+	t_token	*new;
+	char	*s;
+	char	*q_s;
+	char	*d_s;
+
+	new = malloc(sizeof(t_token));
+	if (new == NULL)
+		exit(1);
+	new->next = NULL;
+	s = NULL;
+	while (**end && !ft_strchr(" |><", **end))
+	{
+		if (**end && !ft_strchr("\"\'$", **end)) // 일반문자인 경우
+		{
+			while (**end && !ft_strchr_ig_blsh(" \"\'$|><", *end, end)) // 일반문자 사이 offset 구함.
+				++(*end);
+			s = ft_strnjoin(s, *start, *end - *start); // 일반문자열 join.
+			*start = *end;
+		}
+		if (**end && ft_strchr("\"\'", **end)) // quote인 경우
+		{
+			++(*end);
+			move_quote_end(start, end); // quote 사이 offset 구함.
+			if (**end == 0) // unclosed quote일 경우 예외 처리.
+			{
+				free(s);
+				free(new);
+				return (NULL);
+			}
+			else
+			{
+				q_s = split_word_in_quote(start, end, token); // 환경변수 정보 포함 quote안의 내용 받아옴.
+				s = ft_strnjoin(s, q_s, ft_strlen(q_s)); // quot문자열 join
+				free(q_s);
+			}
+			*start = *end;
+		}
+		if (**end && **end == '$') // $인 경우
+		{
+			d_s = split_word_in_dollar(start, end, token); // 환경변수 정보를 받아옴.
+			s = ft_strnjoin(s, d_s, ft_strlen(d_s)); // 해당 값을 join.
+			free(d_s);
+			*start = *end;
+		}
+	}
+	if (s == NULL)
+		return (NULL);
+	new->s = s;
+	*token |= T_WORD;
+	new->type = ARGS;
+	new->token = *token;
+	if (**end == 0)
+		*start = *end;
+	else
+		*start = *end + 1;
+	return (new);
+}
+
+void	get_redirect_token(char **start, char **end, enum e_token *token)
+{
+	if (*(*end + 1) && *(*end + 1) == **end)
+	{
+		if (**end == '>')
+			*token |= T_RE_APPEND_OUTPUT;
+		else
+			*token |= T_RE_HEREDOC;
+		++(*end);
+	}
+	else
+	{
+		if (**end == '>')
+			*token |= T_RE_OUTPUT;
+		else
+			*token |= T_RE_INPUT;
+	}
+	++(*end);
+	*start = *end;
+}
+
+t_token	*split_pipe(char **end, enum e_token *token)
+{
+	t_token		*new;
+
+	new = malloc(sizeof(t_token));
+	if (new == NULL)
+		exit(0);
+	new->next = NULL;
+	new->s = ft_strndup(*end, 1);
+	if (new->s == NULL)
+		exit(0);
+	*token |= T_PIPE;
+	new->type = PIPE;
+	new->token = *token;
+	++(*end);
+	return (new);
+}
+
+t_token	*split_star(char **end, enum e_token *token)
+{
+	t_token		*new;
+
+	new = malloc(sizeof(t_token));
+	if (new == NULL)
+		exit(0);
+	new->next = NULL;
+	new->s = ft_strndup(*end, 1);
+	if (new->s == NULL)
+		exit(0);
+	*token |= T_STAR;
+	new->type = STAR;
+	new->token = *token;
+	++(*end);
+	return (new);
+}
+
+void	moving_two_pointers(char **start, char **end)
+{
+	while (**end && is_space(**end))
+		++(*end);
+	*start = *end;
+}
+
+t_token	*split_redirect(char **start, char **end, enum e_token *token)
+{
+	t_token	*new;
+
+	get_redirect_token(start, end, token);
+	moving_two_pointers(start, end);
+	if (ft_strchr("|", **start))
+		new = split_pipe(end, token);
+	else if (ft_strchr("*", **start))
+		new = split_star(end, token);
+	else
+		new = split_word(start, end, token);
+	new->type |= REDIRECT;
+	new->type &= ~ARGS;
+	return (new);
+}
+
+int	tokenizing(char *line, t_token *t)
+{
+	char			*start;
+	char			*end;
+	enum e_token	token;
+	t_token			*new;
+	t_token			*p;
+
+	end = line;
+	start = end;
+	p = t;
+	while (*end)
+	{
+		token = T_INIT;
+		moving_two_pointers(&start, &end);
+		if (*start)
+		{
+			if (ft_strchr("><", *start))
+				new = split_redirect(&start, &end, &token);
+			else if (ft_strchr("|", *start))
+				new = split_pipe(&end, &token);
+			else if (ft_strchr("*", *start))
+				new = split_star(&end, &token);
+			else // word, quote, $
+				new = split_word(&start, &end, &token);
+			if (new == NULL)
+				return (WRONG_ACTION);
+			p->next = new;
+			p = p->next;
+		}
+	}
+	return (OK);
+}
